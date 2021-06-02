d1407b9ad91af8829e0ab9e3a7740cb192c6ce5b
pokeninja66 <georgimalamov97@gmail.com>
2021-06-02 21:58:56 +0300

    404 page design

--- 404.php
+++ 404.php
@@ -1,3 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-echo "404 page";
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Page Not Found - 404</title>
+    <meta name="description" content="This page does not exist - 404 error" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style type="text/css">
+        body {
+            background-image: linear-gradient(to top,#0d1117,#15202b,#173041,#144157,#00536d);
+        }
+
+        section {
+            color: white;
+            border-radius: 1em;
+            padding: 1em;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%)
+        }
+    </style>
+</head>
+
+<body>
+    <section>
+        <h1>404 error</h1>
+        <h2>Page not found.</h2>
+    </section>
+</body>
+
+</html>
